b481d9f1e5fb587f93d70b9dbf7511f9503290af
g.maksutenko <g.maksutenko@indevstudio.com>
2021-11-15 13:57:53 +0300

    fix #16 - create template if there no saved config

--- src/Load.nim
+++ src/Load.nim
@@ -1,6 +1,26 @@
 import json, os
 
-const SAVE_DIR_NAME = ".config" / "Dela" / "state.json"
+const SAVE_DIR_NAME = ".config" / "Dela"
+const SAVE_FILE_NAME = "state.json"
+const FULL_PATH = SAVE_DIR_NAME / SAVE_FILE_NAME
+const DEFAULT_CONTENT = """
+[
+  {
+    "Main": [
+      {
+        "TODO": [
+          {
+            "name": "Todo example",
+            "time": 0,
+            "note": "",
+            "done": false
+          }
+        ]
+      }
+    ]
+  }
+]
+"""
 
 type PageSave* = object
   pageName*: string
@@ -16,7 +36,15 @@ type TaskSave* = object
   note*: string  
   done*: bool  
 
-proc readSaveFromFS*(): JsonNode = (getHomeDir() / SAVE_DIR_NAME).readFile.parseJson
+
+proc readSaveFromFS*(): JsonNode = 
+  let path = (getHomeDir() / FULL_PATH)
+  if not fileExists(path):
+    createDir(getHomeDir() / SAVE_DIR_NAME)
+    echo "try to write to: ", path
+    writeFile(path, DEFAULT_CONTENT)
+    
+  path.readFile.parseJson
 
 func getPages*(x: JsonNode): seq[PageSave] = 
   assert x.kind == JArray
